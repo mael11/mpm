5102d44eb5becc6296ee74bc333cf5a6750ab697
mael11 <ismailbaharuddin9@gmail.com>
2018-11-22 17:40:08 +0800

    Create lokasi.js

---
+++ lokasi.js
@@ -0,0 +1,6 @@
+<script>
+// KORDINAT 
+navigator.geolocation.getCurrentPosition((loc) => {
+  console.log('The location in lat lon format is: [', loc.coords.latitude, ',', loc.coords.longitude, ']');
+
+</script>
