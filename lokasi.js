4ad59f19c1fa118476a79cc97f80eff73599efeb
mael11 <ismailbaharuddin9@gmail.com>
2018-11-23 11:07:57 +0800

    Update and rename lokasi.html to lokasi.js

---
+++ lokasi.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+	    // KORDINAT 
+navigator.geolocation.getCurrentPosition((loc) => {
+  //console.log('The location in lat lon format is: [', loc.coords.latitude, ',', loc.coords.longitude, ']');
+  $("#lokasi").html( loc.coords.latitude+','+loc.coords.longitude);
+})
+});
