e9c86db5e41b0db4ddfe405a65f3985f6836458f
mael11 <ismailbaharuddin9@gmail.com>
2018-11-23 11:43:09 +0800

    Update lokasi.js

--- lokasi.js
+++ lokasi.js
@@ -1,6 +1,6 @@
 
 	    // KORDINAT 
-navigator.geolocation.watchPosition()((loc) => {
+navigator.geolocation.getCurrentPosition()((loc) => {
   //console.log('The location in lat lon format is: [', loc.coords.latitude, ',', loc.coords.longitude, ']');
   $("#lokasi").html( loc.coords.latitude+','+loc.coords.longitude);
 })
